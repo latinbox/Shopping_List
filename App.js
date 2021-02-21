3b566e4fa135f7826444335d364dcc2e09d39700
Camilo <camilo.gonzalez@bailacthor.com>
2021-02-21 20:18:40 -0300

    using useState and uuid

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import Header from './components/Header';
+import {uuid} from 'uuidv4';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,6 +10,13 @@ const styles = StyleSheet.create({
 });
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), test: 'Milk'},
+    {id: uuid(), test: 'Eggs'},
+    {id: uuid(), test: 'Tomatoes'},
+    {id: uuid(), test: 'Butter'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header title="Shopping List" />
