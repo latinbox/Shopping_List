f9d422f0433bb23736a0beedbfabb63f501b9e55
Camilo <camilo.gonzalez@bailacthor.com>
2021-02-22 11:12:14 -0300

    delete vector icons applied

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 const styles = StyleSheet.create({
+  listItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  listItemView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  listItemText: {
+    fontSize: 12,
+  },
 });
 
 const ListItem = ({item}) => {
   return (
     <TouchableOpacity style={styles.listItem}>
-      <View style={styles.lisItemView}>
-          <Text style={styles.listItemText}>{item.text}</Text>
+      <View style={styles.listItemView}>
+        <Text style={styles.listItemText}>{item.text}</Text>
+        <Icon name="remove" size={20} color="firebrick" />
       </View>
     </TouchableOpacity>
   );
